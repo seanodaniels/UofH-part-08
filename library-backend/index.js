83764a7a0c200267fb9b38085c3c6ab6f651da49
Sean O'Daniels <sodaniels@gmail.com>
2024-02-08 12:11:03 -0500

    Solution for exercise 8.1.

--- library-backend/index.js
+++ library-backend/index.js
@@ -28,19 +28,11 @@ let authors = [
 ]
 
 /*
- * Suomi:
- * Saattaisi olla järkevämpää assosioida kirja ja sen tekijä tallettamalla kirjan yhteyteen tekijän nimen sijaan tekijän id
- * Yksinkertaisuuden vuoksi tallennamme kuitenkin kirjan yhteyteen tekijän nimen
- *
  * English:
  * It might make more sense to associate a book with its 
  * author by storing the author's id in the context of the 
  * book instead of the author's name However, for simplicity, 
  * we will store the author's name in connection with the book
- *
- * Spanish:
- * Podría tener más sentido asociar un libro con su autor almacenando la id del autor en el contexto del libro en lugar del nombre del autor
- * Sin embargo, por simplicidad, almacenaremos el nombre del autor en conección con el libro
 */
 
 let books = [
@@ -94,10 +86,6 @@ let books = [
     genres: ['classic', 'revolution']
   },
 ]
-
-/*
-  you can remove the placeholder query once your first one has been implemented 
-*/
 
 const typeDefs = `
   type Query {
