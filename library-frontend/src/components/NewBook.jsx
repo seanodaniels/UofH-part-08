2e89109c6b7c4d8354b66ce92d76df8b5b725218
Sean O'Daniels <sodaniels@gmail.com>
2024-02-13 16:04:55 -0500

    Solution for exercise 8.10. Functionality for adding books and refreshing appropriate views.

--- library-frontend/src/components/NewBook.jsx
+++ library-frontend/src/components/NewBook.jsx
@@ -1,6 +1,6 @@
 import { useState } from "react"
 import { gql, useMutation } from "@apollo/client"
-import { CREATE_BOOK } from "../queries"
+import { CREATE_BOOK, ALL_BOOKS, ALL_AUTHORS } from "../queries"
 
 const NewBook = (props) => {
   const [title, setTitle] = useState("")
@@ -9,7 +9,9 @@ const NewBook = (props) => {
   const [genre, setGenre] = useState("")
   const [genres, setGenres] = useState([])
 
-  const [createBook] = useMutation(CREATE_BOOK)
+  const [createBook] = useMutation(CREATE_BOOK, {
+    refetchQueries: [{ query: ALL_BOOKS }, { query: ALL_AUTHORS }],
+  })
 
   const submit = async (event) => {
     event.preventDefault()
